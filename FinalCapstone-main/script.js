ee3151026b0a99c21b7a96a76e4a98a8bf010ea1
TylerDuffie1 <116038387+TylerDuffie1@users.noreply.github.com>
2023-05-26 10:43:17 -0400

    Add files via upload

---
+++ FinalCapstone-main/script.js
@@ -0,0 +1,38 @@
+
+
+// Initialize audio objects with audio file paths
+const audioHit = new Audio('../assets/15.mp3'); // Sound effect audio
+const song = new Audio('../assets/15.mp3'); // Background music audio
+
+/**
+
+Function to play an audio object
+@param {object} audioObject - The audio object to play
+*/
+function playAudio(audioObject) {
+audioObject.play();
+}
+/**
+
+Function to loop an audio object
+@param {object} audioObject - The audio object to loop
+*/
+function loopAudio(audioObject) {
+audioObject.loop = true;
+playAudio(audioObject);
+}
+/**
+
+Function to stop an audio object
+@param {object} audioObject - The audio object to stop
+*/
+function stopAudio(audioObject) {
+audioObject.pause();
+}
+/**
+
+Function to play the background music
+*/
+function play() {
+playAudio(song);
+}
